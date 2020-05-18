78fe9098019897aa73be3ff38b05b6b8c5ccb40b
Juan Francisco Aguirre <jf_aguirre@hotmail.com>
2020-05-18 06:59:30 -0300

    Actualizacion de la clase alumno con constructores y propiedades

--- Alumno.cs
+++ Alumno.cs
@@ -8,20 +8,42 @@ namespace EjercicioFormsAlumno
 {
     class Alumno
     {
-        // Propiedades
+        // Propiedades autoimplementadas
 
         public int Legajo { get; set; }
+
         public string Nombre { get; set; }
+
         public string Apellido { get; set; }
+
         public int TipoDoc { get; set; }
+
         public int NumDoc { get; set; }
+
+        public int Genero { get; set; }
+
         public int Turno { get; set; }
+
         public int Carrera { get; set; }
+
         public int Modalidad { get; set; }
+
         public double Nota1 { get; set; }
+
         public double Nota2 { get; set; }
+
         public double Nota3 { get; set; }
 
+        // Constructores
+
+        public Alumno()
+        {
+            Nombre = Apellido = "N/D";
+            Legajo = TipoDoc = NumDoc = Genero = Turno = Carrera = Modalidad = 0;
+            Nota1 = Nota2 = Nota3 = 0;
+        }
+
+        // Metodos
 
 
 
